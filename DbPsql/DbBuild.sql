b87bce0d3d627c89ad488e5295fb9c385cd8d483
Thomas Beck <T.Beck1@outlook.com>
2024-04-30 07:09:35 -0700

    start of db build

--- DbPsql/DbBuild.sql
+++ DbPsql/DbBuild.sql
@@ -3,18 +3,37 @@ CREATE DATABASE Raven;
 CREATE SCHEMA Materials;
 CREATE SCHEMA Distillation;
 
-CREATE TABLE Materials.Material{
-    MaterialNumber INT PRIMARY KEY,
-    MaterialName VARCHAR(25),
-    Binomial VARCHAR(6),
+CREATE TABLE Materials.Material(
+    MaterialNumber INT NOT NULL PRIMARY KEY,
+    MaterialCode VARCHAR(3) NOT NULL,
+    BatchManaged BIT DEFAULT 0::bit NOT NULL,
+    SequenceId INT NOT NULL,
+    TotalRecords INT NOT NULL,
+    UnitOfIssue VARCHAR(3)
+)
+CREATE TABLE Materials.HighPurityMaterial (
+    MaterialNumber INT NOT NULL PRIMARY KEY,
+    MaterialName VARCHAR(25) NOT NULL,
+    Binomial VARCHAR(6) NOT NULL,
     PermitNumber VARCHAR(25),
-    BatchManaged BIT,
-    MaterialCode,
-    SequenceId,
-    MaxSequenceId,
-}
-CREATE TABLE Materials.MaterialVendor{
-    MaterialNumber INT PRIMARY KEY,
+    MaterialNumber INT NOT NULL FOREIGN KEY REFERENCES Materials.Material,
+);
+CREATE TABLE Materials.RawMaterialVendor (
+    VendorName VARCHAR(25) NOT NULL PRIMARY KEY,
+    VendorLotNumber VARCHAR(25) NOT NULL,
+    ContainerNumberRequired BIT DEFAULT 0::BIT NOT NULL,
+    MaterialNumber INT NOT NULL FOREIGN KEY REFERENCES Materials.Material,
+    ParentMaterialNumber INT NOT NULL FOREIGN KEY REFERENCES Materials.HighPurityMaterial
+);
+CREATE TABLE Material.RawMaterialLog(
+    ProductLotNumber VARCHAR(10) NOT NULL PRIMARY KEY,
+    ProductBatchNumber INT,
     VendorName VARCHAR(25),
     VendorLotNumber VARCHAR(25),
-}
+    SampleId INT,
+    InspectionLotNumber INT,
+    ContainerNumber VARCHAR(7),
+    NetWeight INT,
+    IssueDate DATETIME,
+    MaterialNumber INT FOREIGN KEY REFERENCES Materials.RawMaterialVendor
+);
