db9be491182ef839c9e3417176e95aad7f7f8e4f
Thomas Beck <98434241+BeckTJ@users.noreply.github.com>
2024-05-07 02:33:17 -0700

    Init Database Automation (#2)
    * delete workflow from dev
    
    * Delete .github/workflows directory
    
    * add docker file for postgress (in work)
    
    * init push DbPsql
    
    * start of db build
    
    * Create DB with Data
    
    * create docker compose file
    
    * start db build automation
    
    * remove entrypoint and import files
    
    * edit
    
    * update db build
    
    * update db build 2
    
    * update db build 3
    
    * update db build 4
    
    * update db build 5

---
+++ DbPsql/Data/DataUpload.sql
@@ -0,0 +1,20 @@
+\copy Materials.HighPurityMaterial from 'MaterialData.csv' delimiter ',' csv header;
+
+    create temporary table vendor(
+        MaterialNumber INT NOT NULL PRIMARY KEY,
+        VendorName VARCHAR(25) NOT NULL,
+        MaterialCode VARCHAR(3) NOT NULL,
+        BatchManaged BIT DEFAULT 0::bit,
+        ContainerNumberRequired BIT DEFAULT 0::bit,
+        SequenceId INT NOT NULL,
+        TotalRecords INT NOT NULL,
+        UnitOfIssue VARCHAR(3),
+        ParentMaterialNumber INT NOT NULL
+    );
+
+    \copy vendor from 'MaterialVendor.csv' delimiter ',' csv header;
+    
+    INSERT INTO Materials.RawMaterialVendor
+    (MaterialNumber,VendorName,MaterialCode,BatchManaged,ContainerNumberRequired,SequenceId,TotalRecords,UnitOfIssue,ParentMaterialNumber)
+    SELECT MaterialNumber, VendorName, MaterialCode, BatchManaged, ContainerNumberRequired, SequenceId, TotalRecords, UnitOfIssue,
+    (SELECT MaterialNumber From Materials.HighPurityMaterial WHERE MaterialNumber = ParentMaterialNumber) from vendor;
---
+++ DbPsql/DbBuild.sql
@@ -0,0 +1,49 @@
+CREATE USER RavenAdmin;
+
+CREATE DATABASE Raven;
+
+GRANT ALL PRIVILEGES ON DATABASE Raven TO RavenAdmin;
+
+\c raven
+
+CREATE SCHEMA Materials;
+CREATE SCHEMA Distillation;
+
+CREATE TABLE Materials.HighPurityMaterial
+(
+    MaterialNumber INT NOT NULL PRIMARY KEY,
+    MaterialName VARCHAR(25) NOT NULL,
+    Binomial VARCHAR(6) NOT NULL,
+    PermitNumber VARCHAR(25),
+    MaterialCode VARCHAR(3) NOT NULL,
+    BatchManaged BIT DEFAULT 0::bit NOT NULL,
+    SequenceId INT NOT NULL,
+    TotalRecords INT NOT NULL,
+    UnitOfIssue VARCHAR(3)
+);
+CREATE TABLE Materials.RawMaterialVendor 
+(
+    MaterialNumber INT NOT NULL PRIMARY KEY,
+    VendorName VARCHAR(25) NOT NULL,
+    MaterialCode VARCHAR(3) NOT NULL,
+    BatchManaged BIT DEFAULT 0::BIT NOT NULL,
+    ContainerNumberRequired BIT DEFAULT 0::BIT NOT NULL,
+    SequenceId INT NOT NULL,
+    TotalRecords INT NOT NULL,
+    UnitOfIssue VARCHAR(3),
+    ParentMaterialNumber INT NOT NULL REFERENCES Materials.HighPurityMaterial(MaterialNumber)
+);
+CREATE TABLE Distillation.RawMaterialLog
+(
+    ProductLotNumber VARCHAR(10) NOT NULL PRIMARY KEY,
+    ProductBatchNumber INT,
+    VendorName VARCHAR(25) NOT NULL,
+    VendorLotNumber VARCHAR(25),
+    SampleId INT,
+    InspectionLotNumber INT,
+    ContainerNumber VARCHAR(7),
+    IssueDate DATE NOT NULL,
+    NetWeight INT DEFAULT 180,
+    MaterialNumber INT NOT NULL REFERENCES Materials.RawMaterialVendor(MaterialNumber)
+)
+\i ../raven/DataUpload.sql;
