92766d098f529eef312c357d78d6cd226036bf39
Thomas Beck <t.beck1@outlook.com>
2024-05-21 16:41:27 -0700

    remove connection string from Entities

--- Entities/ravenContext.cs
+++ Entities/ravenContext.cs
@@ -22,12 +22,6 @@ namespace Entities
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Host=localhost;Database=raven; Username=postgres;Password=password");
-
-            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
