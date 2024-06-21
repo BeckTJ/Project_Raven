9c67157e6a0f7f75d07b8dfb5f4964af1b4724b9
Thomas Beck <t.beck1@outlook.com>
2024-06-21 16:12:20 -0700

    kotlin

--- raven/composeApp/src/androidMain/kotlin/App.kt
+++ raven/composeApp/src/androidMain/kotlin/App.kt
@@ -1,13 +1,17 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -18,22 +22,15 @@ import raven.composeapp.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     MaterialTheme {
-        val greeting = remember { Greeting.greet() }
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent)
-            {
-                greeting.forEach { greeting ->
-                    Text{greeting}
-                    Divider()
-            }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+        val greeting = remember { Greeting().greet() }
+
+        Column(
+            modifier = Modifier.padding(all = 20.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            greeting.forEach { greeting ->
+                Text(greeting)
+                Divider()
             }
         }
     }
