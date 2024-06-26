277dfdd498b33f80bcf47a3f90fe1b2985a74c21
Thomas Beck <t.beck1@outlook.com>
2024-06-26 08:45:04 -0700

    working on frontend

--- raven/composeApp/src/androidMain/kotlin/App.kt
+++ raven/composeApp/src/androidMain/kotlin/App.kt
@@ -1,34 +1,26 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import raven.composeapp.generated.resources.Res
-import raven.composeapp.generated.resources.compose_multiplatform
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-@Preview
-fun App() {
+fun App(mainViewModel: MainViewModel = viewModel()) {
     MaterialTheme {
-        val greeting = remember { Greeting().greet() }
+        val greetings by mainViewModel.greetingList.collectAsStateWithLifecycle()
 
         Column(
             modifier = Modifier.padding(all = 20.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
-            greeting.forEach { greeting ->
+            greetings.forEach { greeting ->
                 Text(greeting)
                 Divider()
             }
--- raven/composeApp/src/androidMain/kotlin/MainViewModel.kt
+++ raven/composeApp/src/androidMain/kotlin/MainViewModel.kt
@@ -1,6 +1,18 @@
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
 class MainViewModel : ViewModel(){
-    private val _greetingList = MutableStateFLow<List<String>>(listOf())
-    val greetingList : StateFLow<List<String>> get() = _greetingList
+    private val _greetingList = MutableStateFlow<List<String>>(listOf())
+    val greetingList : StateFlow<List<String>> get() = _greetingList
+    init {
+        viewModelScope.launch {
+            Greeting().greet().collect { phrase ->
+                _greetingList.update { list -> list + phrase }
+            }
+        }
+    }
 }
