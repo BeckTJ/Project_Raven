913ffbf42b371b13503bae4a98f1d7c9a2061f56
Thomas Beck <t.beck1@outlook.com>
2024-11-15 06:56:41 -0800

    update

--- DbPsql/GetNextDrumId.sql
+++ DbPsql/GetNextDrumId.sql
@@ -4,11 +4,20 @@ AS
 Declare 
     lotNumber varchar(10)
     id int
+    code varchar(3)
 BEGIN
     lotNumber = Select Product_Lot_Number From Distillation.Raw_Material_Log Where material_Number = 32716 Order By Product_Lot_Number Desc Limit 1;
     
     If(lotNumber is null)
-        Select Concat(sequence_id,Material_Code) From Materials.Raw_Material_Vendor Where material_Number = 32716; --36178 -> no drums issued
+        return Select Concat(sequence_id,Material_Code) From Materials.Raw_Material_Vendor Where material_Number = 32716; --36178 -> no drums issued
 
     ELSE  
-        if(lotNumber) 
+        if(length(lotNumber) == 5 || length(lotNumber) == 9)
+            id = SUBSTRING(lotNumber,1,3);
+            code = SUBSTRING(lotNumber,4,2);
+            return CONCAT(id+1,code);
+        if(length(lotNumber)== 6 || length(lotNumber) == 10)
+            id = SUBSTRING(lotNumber,1,4);
+            code = SUBSTRING(lotNumber,5,2);
+            return CONCAT(id+1,code);
+END
